3e419f5c15afe8539e90395823578cb6bbf3aa0f
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-06-14 10:52:57 +0600

    patch added

--- index.js
+++ index.js
@@ -88,6 +88,22 @@ async function run() {
 
     })
 
+
+    app.patch('/users/admin/:id', async(req,res) =>{
+      const id = req.params.id;
+      console.log(id);
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+         role: 'admin'
+        },
+      };
+ 
+        const result = await usersCollection.updateOne(filter,updateDoc);
+
+        res.send(result);
+    })
+
     app.delete('/carts/:id',async(req ,res) =>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
