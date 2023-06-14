19490475f21d6ef9da377c4c5bb872bc2bc22875
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-06-14 22:47:21 +0600

    admin role added

--- index.js
+++ index.js
@@ -129,6 +129,23 @@ async function run() {
     })
 
 
+    // security layout
+      app.get('/users/admin/:email', verifyJWT ,async(req,res) =>{
+
+        const email = req.params.email;
+          if(req.decoded.email !== email){
+            res.send({admin:false})
+          }
+
+
+         const query  = {email:email}
+         const user = await usersCollection.findOne(query);
+         const result = {admin:user?.role === 'admin'}
+         res.send(result);
+      })
+
+
+
     app.patch('/users/admin/:id', async(req,res) =>{
       const id = req.params.id;
       console.log(id);
